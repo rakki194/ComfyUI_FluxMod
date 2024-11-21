17105634b6242573aa1fa470f198c0eb77a5de30
lodestone-rock <lodestone.rock@gmail.com>
2024-11-21 09:53:55 +0700

    bug fix for true W8A16 inference, add custom sampler for switching fp16 and bf16 on the fly

--- flux_mod/loader.py
+++ flux_mod/loader.py
@@ -56,14 +56,27 @@ def load_selected_keys(filename, exclude_keywords=[]):
     return tensors
 
 
-def cast_layers(module, layer_type, dtype):
+def cast_layers(module, layer_type, dtype, exclude_keywords=[]):
+    """Casts layers in a module to the specified dtype, excluding layers with names containing keywords.
+
+    Args:
+        module (nn.Module): The module containing layers to cast.
+        layer_type (type): The type of layers to cast (e.g., nn.Linear).
+        dtype (torch.dtype): The target data type for casting.
+        exclude_keywords (list, optional): A list of keywords to exclude from casting based on layer names. Defaults to [].
+
+    Returns:
+        None
+    """
+
     for child_name, child in module.named_children():
-        if isinstance(child, layer_type):
-            # Cast to the specified dtype
-            child.to(dtype=dtype)
+        if isinstance(child, layer_type) and not any(keyword in child_name for keyword in exclude_keywords):
+            # Cast the layer only if it's the desired type and not excluded
+            # omit bias
+            child.weight.data = child.weight.data.to(dtype=dtype)
         else:
             # Recursively apply to child modules
-            cast_layers(child, layer_type, dtype)
+            cast_layers(child, layer_type, dtype, exclude_keywords)
 
 
 def load_flux_mod(model_path, timestep_guidance_path, linear_dtypes=torch.bfloat16):
@@ -107,12 +120,13 @@ def load_flux_mod(model_path, timestep_guidance_path, linear_dtypes=torch.bfloat
     model.diffusion_model = FluxMod(params=params)
 
     model.diffusion_model.load_state_dict(state_dict)
-    cast_layers(model.diffusion_model, nn.Linear, dtype=linear_dtypes)
     model.diffusion_model.distilled_guidance_layer = Approximator(64, 3072, 5120, 4)
     model.diffusion_model.distilled_guidance_layer.load_state_dict(timestep_state_dict)
     model.diffusion_model.dtype = unet_dtype
     model.diffusion_model.eval()
     model.diffusion_model.to(unet_dtype)
+    # we cast to fp8 for mixed matmul ops but omit the picky and sensitive layers
+    cast_layers(model.diffusion_model, nn.Linear, dtype=linear_dtypes, exclude_keywords=["img_in", "final_layer", "scale"])
     model.diffusion_model
 
     model_patcher = comfy.model_patcher.ModelPatcher(
--- flux_mod/nodes.py
+++ flux_mod/nodes.py
@@ -4,6 +4,9 @@ import torch
 import folder_paths
 
 from .loader import load_flux_mod
+from .sampler import common_ksampler
+import comfy.samplers 
+
 
 class FluxModCheckpointLoader:
     @classmethod
@@ -39,6 +42,76 @@ class FluxModCheckpointLoader:
         )
         return (flux_mod,)
 
+
+class ModelMover:
+    @classmethod
+    def INPUT_TYPES(s):
+        return {
+            "required": {
+                "samples": ("MODEL", ),
+            }
+        }
+    
+
+    RETURN_TYPES = ("MODEL",)
+    RETURN_NAMES = ("model",)
+    FUNCTION = "load_checkpoint"
+    CATEGORY = "ExtraModels/FluxMod"
+    TITLE = "FluxModCheckpointLoader"
+
+    def load_checkpoint(self, ckpt_name, guidance_name, quant_mode):
+        dtypes = {
+            "bf16": torch.bfloat16, 
+            "float8_e4m3fn (8 bit)": torch.float8_e4m3fn, 
+            "float8_e5m2 (also 8 bit)": torch.float8_e5m2
+        }
+            
+        ckpt_path = folder_paths.get_full_path("checkpoints", ckpt_name)
+        guidance_path = folder_paths.get_full_path("checkpoints", guidance_name)
+        flux_mod = load_flux_mod(
+            model_path = ckpt_path,
+            timestep_guidance_path = guidance_path,
+            linear_dtypes=dtypes[quant_mode]
+        )
+        return (flux_mod,)
+    
+
+
+class KSamplerMod:
+    @classmethod
+    def INPUT_TYPES(s):
+        return {
+            "required": {
+                "model": ("MODEL", {"tooltip": "The model used for denoising the input latent."}),
+                "seed": ("INT", {"default": 0, "min": 0, "max": 0xffffffffffffffff, "tooltip": "The random seed used for creating the noise."}),
+                "steps": ("INT", {"default": 20, "min": 1, "max": 10000, "tooltip": "The number of steps used in the denoising process."}),
+                "cfg": ("FLOAT", {"default": 8.0, "min": 0.0, "max": 100.0, "step":0.1, "round": 0.01, "tooltip": "The Classifier-Free Guidance scale balances creativity and adherence to the prompt. Higher values result in images more closely matching the prompt however too high values will negatively impact quality."}),
+                "sampler_name": (comfy.samplers.KSampler.SAMPLERS, {"tooltip": "The algorithm used when sampling, this can affect the quality, speed, and style of the generated output."}),
+                "scheduler": (comfy.samplers.KSampler.SCHEDULERS, {"tooltip": "The scheduler controls how noise is gradually removed to form the image."}),
+                "positive": ("CONDITIONING", {"tooltip": "The conditioning describing the attributes you want to include in the image."}),
+                "negative": ("CONDITIONING", {"tooltip": "The conditioning describing the attributes you want to exclude from the image."}),
+                "latent_image": ("LATENT", {"tooltip": "The latent image to denoise."}),
+                "denoise": ("FLOAT", {"default": 1.0, "min": 0.0, "max": 1.0, "step": 0.01, "tooltip": "The amount of denoising applied, lower values will maintain the structure of the initial image allowing for image to image sampling."}),
+                "activation_casting": (["bf16", "fp16"], {"default": "bf16", "tooltip": "cast model activation to bf16 or fp16, always use bf16 unless your card does not supports it"})
+            }
+        }
+
+    RETURN_TYPES = ("LATENT",)
+    OUTPUT_TOOLTIPS = ("The denoised latent.",)
+    FUNCTION = "sample"
+    CATEGORY = "ExtraModels/FluxMod"
+    DESCRIPTION = "Uses the provided model, positive and negative conditioning to denoise the latent image."
+    TITLE = "KSamplerMod"
+
+    def sample(self, model, seed, steps, cfg, sampler_name, scheduler, positive, negative, latent_image, denoise=1.0, activation_casting="bf16"):
+        dtypes = {
+            "bf16": torch.bfloat16, 
+            "fp16": torch.float16
+        }
+        with torch.autocast(device_type="cuda", dtype=dtypes[activation_casting]):
+            return common_ksampler(model, seed, steps, cfg, sampler_name, scheduler, positive, negative, latent_image, denoise=denoise)
+    
 NODE_CLASS_MAPPINGS = {
     "FluxModCheckpointLoader" : FluxModCheckpointLoader,
+    "KSamplerMod": KSamplerMod,
 }
